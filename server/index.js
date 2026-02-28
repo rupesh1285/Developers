43ec0542ab1dca372b88432473c5311e2a8f020d
Rupesh Agarwal <rupeshagarwal728@gmail.com>
2026-03-01 00:51:01 +0530

    Initial backend setup with authentication

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
