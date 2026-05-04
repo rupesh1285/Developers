2fe1c23143e8f575c1163cfeeb23d9504f3ee972
Rupesh Agarwal <rupeshagarwal728@gmail.com>
2026-05-04 21:47:40 +0530

    Improving SEO Performance Score

--- finalist-client/vite.config.js
+++ finalist-client/vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext',
+    minify: 'terser', // Terser is slightly better at dead-code elimination than esbuild
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        }
+      }
+    }
+  },
   server: {
     proxy: {
       '/api': {
