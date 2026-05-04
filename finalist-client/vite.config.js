f95656e20a4d74673e6d654dbcf3e72fba2035dd
Rupesh Agarwal <rupeshagarwal728@gmail.com>
2026-05-04 22:10:34 +0530

    LOL, I m Tired

--- finalist-client/vite.config.js
+++ finalist-client/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
   build: {
     target: 'esnext',
-    minify: 'terser', 
+    // 🌟 THE FIX: Removed `minify: 'terser'`. Vite will now default to its built-in esbuild!
     cssCodeSplit: true,
     rollupOptions: {
       output: {
